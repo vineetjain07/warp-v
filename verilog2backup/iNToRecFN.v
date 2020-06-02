9044ef929e962637a1b77f862824e2cce9715312
Vineet Jain <32877048+vineetjain07@users.noreply.github.com>
2020-06-02 17:10:45 +0530

    Update iNToRecFN.v

--- verilog2backup/iNToRecFN.v
+++ verilog2backup/iNToRecFN.v
@@ -66,7 +66,7 @@ module
         countLeadingZeros(extAbsIn, adjustedNormDist);
     /*------------------------------------------------------------------------
     *------------------------------------------------------------------------*/
-    assign sig = ({ {(expWidth - 1 - extIntWidth){1'b0}} ,extAbsIn} << adjustedNormDist) >> (extIntWidth - intWidth);
+    assign sig = ({ {( (2 ** (expWidth - 1)) - 1 ){1'b0}} ,extAbsIn} << adjustedNormDist) >> (extIntWidth - intWidth);
     assign isZero = !sig[intWidth - 1];
     assign sExp = {1'b0 ,2'b10, ~adjustedNormDist};
 
