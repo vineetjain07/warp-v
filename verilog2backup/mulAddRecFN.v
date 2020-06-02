90455451194e39a8e11787310062d44ee79de92a
Vineet Jain <32877048+vineetjain07@users.noreply.github.com>
2020-06-02 15:39:13 +0530

    Update mulAddRecFN.v

--- verilog2backup/mulAddRecFN.v
+++ verilog2backup/mulAddRecFN.v
@@ -319,7 +319,7 @@ module
     wire signed [(expWidth + 1):0] notCDom_sExp =
         intermed_sExp - {{ ( (expWidth + 2) - (clog2(prodWidth + 4)) ){1'b0} } , notCDom_nearNormDist};
 
-    wire [(prodWidth*2 + 6):0] notCDom_mainSig_temp1 =  ({ {(prodWidth + 4){1'b0}} ,1'b0, notCDom_absSigSum} << notCDom_nearNormDist) >> (sigWidth - 1);
+ wire [(prodWidth*2 + 6):0] notCDom_mainSig_temp1 =  ({ {(prodWidth + 3){1'b0}} ,1'b0, notCDom_absSigSum} << notCDom_nearNormDist) >> (sigWidth - 1);
     wire [(sigWidth + 4):0] notCDom_mainSig =  notCDom_mainSig_temp1[(sigWidth + 4):0];
 
     wire [(((sigWidth/2 + 1) | 1) - 1):0] CDom_grainAlignedLowReduced2Sig = notCDom_reduced2AbsSigSum[sigWidth/2:0] << ((sigWidth/2) & 1);
